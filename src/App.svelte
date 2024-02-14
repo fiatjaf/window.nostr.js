23fa76a121c82fcf156f101ec15c01291e52643a
fiatjaf <fiatjaf@gmail.com>
2024-02-14 11:31:09 -0300

    creating accounts.

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import debounce from 'debounce'
   import {hexToBytes, bytesToHex} from '@noble/hashes/utils'
   import {
     generateSecretKey,
@@ -12,7 +13,8 @@
     createAccount,
     parseBunkerInput,
     fetchCustodialbunkers as fetchCustodialBunkers,
-    type BunkerPointer
+    type BunkerPointer,
+    type BunkerProfile
   } from 'nostr-tools/nip46'
   import {queryProfile} from 'nostr-tools/nip05'
   import {npubEncode} from 'nostr-tools/nip19'
@@ -21,12 +23,23 @@
   const win = window as any
   const pool = new SimplePool()
   let bunkerInput: HTMLInputElement
+  let nameInput: HTMLInputElement
+  let chosenProvider: BunkerProfile | undefined
+  let clientSecret: Uint8Array
+  const local = localStorage.getItem('nip46ClientSecretKey')
+  if (local) {
+    clientSecret = hexToBytes(local)
+  } else {
+    clientSecret = generateSecretKey()
+    localStorage.setItem('nip46ClientSecretKey', bytesToHex(clientSecret))
+  }
 
   let opened: Boolean
   let bunkerPointer: BunkerPointer | null
   let resolveBunker: (_: BunkerSigner) => void
   let bunker: Promise<BunkerSigner>
   let connecting: boolean
+  let creating: boolean
   let connected: null | {
     pubkey: string
     npub: string
@@ -35,6 +48,7 @@
     event: NostrEvent | null
   }
   let metadataSub: SubCloser | null
+  let providers: BunkerProfile[] = []
   reset()
 
   function reset() {
@@ -44,6 +58,7 @@
       resolveBunker = resolve
     })
     connecting = false
+    creating = false
     connected = null
     metadataSub = null
   }
@@ -51,7 +66,7 @@
   const bunkerSignerParams: BunkerSignerParams = {
     pool,
     onauth(url: string) {
-      console.log('auth', url)
+      window.open(url, 'window.nostr', `width=600,height=800,popup=yes`)
     }
   }
 
@@ -59,7 +74,10 @@
     let data = localStorage.getItem('nip46BunkerPointer')
     if (data) {
       bunkerPointer = JSON.parse(data)
-      connect()
+      connecting = true
+      connect(
+        new BunkerSigner(clientSecret, bunkerPointer!, bunkerSignerParams)
+      )
     }
 
     setTimeout(() => {
@@ -103,7 +121,7 @@
     }
 
     bunkerInput.setCustomValidity('')
-    connect()
+    connect(new BunkerSigner(clientSecret, bunkerPointer!, bunkerSignerParams))
   }
 
   async function handleDisconnect(ev: MouseEvent) {
@@ -113,23 +131,40 @@
     reset()
   }
 
-  async function connect() {
-    connecting = true
+  async function handleOpenCreate(ev: MouseEvent) {
+    ev.preventDefault()
+    creating = true
+    if (providers.length === 0)
+      providers = await fetchCustodialBunkers(pool, ['wss://relay.nostr.band'])
+  }
 
-    let clientSecret
-    const local = localStorage.getItem('nip46ClientSecretKey')
-    if (local) {
-      clientSecret = hexToBytes(local)
-    } else {
-      clientSecret = generateSecretKey()
-      localStorage.setItem('nip46ClientSecretKey', bytesToHex(clientSecret))
-    }
+  async function handleCreate(ev: SubmitEvent) {
+    ev.preventDefault()
+    if (!chosenProvider) return
 
-    let bunker = new BunkerSigner(
-      clientSecret,
-      bunkerPointer!,
-      bunkerSignerParams
+    let bunker = await createAccount(
+      chosenProvider,
+      bunkerSignerParams,
+      nameInput.value,
+      chosenProvider.domain
     )
+    opened = true
+    creating = false
+
+    connect(bunker)
+  }
+
+  const checkNameInput = debounce(async () => {
+    if (chosenProvider && nameInput.value.length > 0) {
+      if (await queryProfile(nameInput.value + '@' + chosenProvider.domain)) {
+        nameInput.setCustomValidity(`'${nameInput.value}' is already taken.`)
+      } else {
+        nameInput.setCustomValidity('')
+      }
+    }
+  }, 500)
+
+  async function connect(bunker: BunkerSigner) {
     await bunker.connect()
 
     // set this so the floating thing will update
@@ -175,7 +210,35 @@
   class="tw-opacity-50 hover:tw-opacity-100 tw-px-4 tw-py-2 tw-transition-all"
   class:tw-cursor-pointer={!connected && !opened}
 >
-  {#if connected}
+  {#if creating}
+    <form class="tw-mt-2 tw-mb-1" on:submit={handleCreate}>
+      <div class="tw-flex items-center">
+        <input
+          class="tw-px-2 tw-py-1 tw-text-lg tw-text-right"
+          placeholder="bob"
+          bind:this={nameInput}
+          on:input={checkNameInput}
+        />
+        <select
+          class="tw-ml-2 tw-px-2 tw-py-1 tw-text-lg"
+          bind:value={chosenProvider}
+        >
+          {#each providers as prov}
+            <option
+              label={prov.domain}
+              value={prov}
+              class="tw-px-2 tw-py-1 tw-text-lg"
+            />
+          {/each}
+        </select>
+      </div>
+      <button
+        class="tw-block tw-w-full tw-mt-2 tw-px-2 tw-py-1 tw-text-lg tw-border-0 tw-bg-indigo-800 tw-hover:bg-indigo-900 tw-cursor-pointer tw-text-white"
+      >
+        create
+      </button>
+    </form>
+  {:else if connected}
     <div class="tw-text-right">
       {#if opened}
         connected as
@@ -208,7 +271,7 @@
       </span>
     </div>
     {#if opened}
-      <form class="tw-my-2" on:submit={handleConnect}>
+      <form class="tw-mt-2 tw-mb-1" on:submit={handleConnect}>
         <input
           class="tw-px-2 tw-py-1 tw-text-lg"
           placeholder="user@provider or bunker://..."
@@ -220,6 +283,12 @@
           connect
         </button>
       </form>
+      <div class="tw-text-right tw-mb-1">
+        <button
+          class="tw-border-0 tw-text-sm tw-cursor-pointer hover:tw-underline"
+          on:click={handleOpenCreate}>create account</button
+        >
+      </div>
     {/if}
   {/if}
 </div>
