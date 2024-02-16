90f42af9d6d1a703c2925d0d9dcc0244c345d8d8
Daniele Tonon <tonon@vitamino.it>
2024-02-17 00:03:14 +0100

    Add an option to support bottom placement

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,32 @@ import App from './App.svelte'
 const div = document.createElement('div')
 document.body.appendChild(div)
 div.id = 'windowNostrModal'
+
+// To move the widget to the bottom add this code before the <script src='...' inclusion
+// --- --- --- --- --- --- --- --- --- 
+// <script>
+//   window.windowNostrJsParams = {
+//     position: 'bottom'
+//   }
+// </script>
+
+const position = (window as any).windowNostrJsParams ? (window as any).windowNostrJsParams.position || 'top' : 'top';
+
 div.classList.add(
   'tw-fixed',
-  'tw-top-6',
   'tw-right-6',
   'tw-animate-fadein'
 )
+
+if (position === 'bottom') {
+  div.classList.add(
+    'tw-bottom-6',
+  )
+} else {
+  div.classList.add(
+    'tw-top-6',
+  )
+}
 
 const app = new App({
   target: div
